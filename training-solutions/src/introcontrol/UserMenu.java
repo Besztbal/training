4172b4ded0b1d0b1e49e002633ee9bccdda04497
Besztbal <89199416+Besztbal@users.noreply.github.com>
2022-04-08 13:19:53 +0200

    20220408

--- training-solutions/src/introcontrol/UserMenu.java
+++ training-solutions/src/introcontrol/UserMenu.java
@@ -1,2 +1,26 @@
-package introcontrol;public class UserMenu {
+package introcontrol;
+
+import java.util.Scanner;
+
+public class UserMenu {
+
+    public static void main(String[] args) {
+        System.out.println("Válasszon az alábbi menüpontok közül:");
+        System.out.println();
+        System.out.println("1. Felhasználók listázása");
+        System.out.println("2. Felhasználó felvétele");
+        System.out.println("Többi: Kilépés");
+
+        Scanner scanner=new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if (number==1){
+            System.out.println("Felhasználók listázása");
+        } else if (number==2){
+            System.out.println("Felhasználó felvétele");
+        } else {
+            System.out.println("");
+        }
+
+    }
 }
